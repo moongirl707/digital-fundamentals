f7c2f73d3cf3ee4081ed4a1c191787a7dd670989
707 <afanasyeva175@gmail.com>
2025-11-07 14:43:18 +0300

    Create analyser4.sh

---
+++ scripts/analyser4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+min=100; max=-1
+for i in $(ls $1)
+do
+  for j in $(cat $1$i)
+  do
+    age=$(echo $j | cut -d ":" -f2)
+    if [$age -lt $min]
+    then
+        min=$age
+    elif [$age -gt $max]
+    then
+        max=$age
+    fi
+  done
+done
+echo $min
+echo $max
